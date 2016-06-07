e3ebd215cd34147493f30a164ad593f9f4fd9f57
Peter Braden <peterbraden@peterbraden.co.uk>
2016-06-07 13:26:50 +0200

    image

--- src/sceneobject.rs
+++ src/sceneobject.rs
@@ -5,7 +5,12 @@ use material::Material;
 use na::Vec3;
 
 pub trait SceneObject {
+
     fn intersects(&self, r: &Ray) -> Option<Intersection>;
+//  fn intersectsP(&self, r: &Ray) -> bool;
+
     fn get_material(&self, point: Vec3<f64>) -> Material;
+
+    // World space bounding box
     fn bounds(&self) -> BBox;
 }
